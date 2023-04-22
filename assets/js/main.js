0f0eb93764d524cea14312a16956be9421d875b7
projectnum17 <egor.chornyy@gmail.com>
2023-04-22 17:27:03 +0300

    feat: "section offer"

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,40 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-const scrollHeader = () =>{
+const scrollHeader = () => {
     const header = document.getElementById('header')
     // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
-    this.scrollY >= 50 ? header.classList.add('scroll-header') 
-                       : header.classList.remove('scroll-header')
+    this.scrollY >= 50 ? header.classList.add('scroll-header')
+        : header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
 
 /*=============== SERVICES MODAL ===============*/
+const modalViews = document.querySelectorAll('.services__modal'),
+    modalBtns = document.querySelectorAll('.services__button'),
+    modalClose = document.querySelectorAll('.services__modal-close')
 
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal');
+    document.body.style.overflow = 'hidden';
+}
 
+modalBtns.forEach((mb, i) => {
+    mb.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalClose.forEach((mc) => {
+    mc.addEventListener('click', () => {
+        modalViews.forEach((mv) => {
+            mv.classList.remove('active-modal')
+            document.body.style.overflow = 'auto';
+        })
+    })
+})
 /*=============== MIXITUP FILTER PORTFOLIO ===============*/
 
 
-/* Link active work */ 
+/* Link active work */
 
 
 /*=============== SWIPER TESTIMONIAL ===============*/
@@ -22,7 +43,7 @@ window.addEventListener('scroll', scrollHeader)
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 
-/*=============== LIGHT DARK THEME ===============*/ 
+/*=============== LIGHT DARK THEME ===============*/
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
