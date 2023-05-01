525cbccef18d9d4b1ff5d830c1a290734cf42c7d
projectnum17 <egor.chornyy@gmail.com>
2023-05-01 17:16:54 +0300

    feat: scroll js

--- assets/js/main.js
+++ assets/js/main.js
@@ -82,11 +82,29 @@ textarea.addEventListener('keyup', function(){
   }
 });
 
-/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+//SCROLL SECTIONS ACTIVE LINK
+const sections = document.querySelectorAll('section[id]')
+    
+const scrollActive = () =>{
+  	const scrollY = window.pageYOffset
+
+	sections.forEach(current =>{
+		const sectionHeight = current.offsetHeight,
+			  sectionTop = current.offsetTop - 58,
+			  sectionId = current.getAttribute('id'),
+			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+		if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+			sectionsClass.classList.add('active-link')
+		}else{
+			sectionsClass.classList.remove('active-link')
+		}                                                    
+	})
+}
+window.addEventListener('scroll', scrollActive)
+
+//LIGHT DARK THEME
 
 
-/*=============== LIGHT DARK THEME ===============*/
-
-
-/*=============== SCROLL REVEAL ANIMATION ===============*/
+//SCROLL REVEAL ANIMATION
 
